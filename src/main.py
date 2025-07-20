1a867624a2205705f201ca4cc44e3b487085f832
moyasser02 <mohamedyasserktb20@gmail.com>
2025-07-21 01:09:36 +0300

    init

--- src/main.py
+++ src/main.py
@@ -1,8 +1,27 @@
-from fastapi import FastAPI
-
+from typing_extensions import Annotated
+from fastapi import FastAPI , Body , Depends
+from sqlalchemy.orm import Session, sessionmaker
+from database.core import engine , SessionLocal
+import todo.model
+from todo.model import Todos
+import users.model
+from users.model import User
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+todo.model.Base.metadata.create_all(bind=engine)
+users.model.Base.metadata.create_all(bind=engine)
 
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+db_dependency = Annotated[Session, Depends(get_db)]
+
+@app.get("/")
+async def read_all(db: db_dependency):
+    todos = db.query(Todos).all()
+    return todos
---
+++ src/users/model.py
@@ -0,0 +1,11 @@
+from database.core import Base 
+from sqlalchemy import Column, Integer, String
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, index=True)
+    email = Column(String, unique=True, index=True)
+    full_name = Column(String, nullable=True)  # Optional field
+    hashed_password = Column(String)  # Store hashed password securely
